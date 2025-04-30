17ad3a9b082510ec17cfc1b0f5f91c9d392ed6ea
jhuereca <jhuereca@smail.uni-koeln.de>
2025-04-30 14:34:22 +0200

    correction

--- Homework1.0/src/exercise1/Textbook.java
+++ Homework1.0/src/exercise1/Textbook.java
@@ -3,7 +3,7 @@ package exercise1;
 public class Textbook extends Book {
 	public String subject;
 	
-	public Textbook(String title,int year,String subject, String Subject) {
+	public Textbook(String title,int year,String subject, String author) {
 		super(title, year, author);
 		
 		this.subject = subject;
