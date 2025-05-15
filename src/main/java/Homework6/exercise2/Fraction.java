157a647f779a930c6a04b604f9c8279b460d4585
jhuereca <jhuereca@smail.uni-koeln.de>
2025-05-15 17:33:51 +0200

    I am tired of this....

--- src/main/java/Homework6/exercise2/Fraction.java
+++ src/main/java/Homework6/exercise2/Fraction.java
@@ -15,12 +15,34 @@ public class Fraction {
 		this.setNenner(this.zaehler);
 		this.setZaehler(this.nenner);
 	}
+	/* 
+	 * public void Kehrwert(){
+	 *  	if(this.zaehler==0){
+	 *  throw new ArithmeticException("Cannot take reciprocal of a fraction with zero numerator (results in zero denominator).");}
+	 *  int tempZahealer = this.zaehler;
+	 *  this.zaehler = this.nenner;
+	 *  this.nenner = tempZaehler; 
+	 *  }
+	 * 
+	 * 
+	 * 
+	 * 
+	 * */
 
 	// wrong
 	public void multiplikation(Fraction a) {
 		this.setNenner(this.nenner * nenner);
 		this.setZaehler(this.zaehler * zaehler);
 	}
+	
+	/* public void multiplikation(Fraction a) {
+	 *   this.zaehler = this.zaehler * a.getZaehler();
+	 *    this.nenner = this.nenner * a.getNenner();
+	 *    if (this.nenner == 0) {
+     *  throw new ArithmeticException("Multiplication resulted in a zero denominator.");
+     *  }
+     *    } */
+	
 
 	// wrong
 	public void addition(Fraction a) {
@@ -40,6 +62,30 @@ public class Fraction {
 		this.zaehler *= ggt;
 		this.nenner *= ggt;
 	}
+	
+	/* 
+	 * public void kuerzen(){
+	 * if(this.nenner == 0){
+	 * throw new ArithmeticException("Cannot simplify a fraction with zero denominator.");}
+	 * if(this.zaehler == 0) {
+	 * this.nenner = 1;
+	 * return;
+	 * }
+	 * int commonDivisor = ggt(Math.abs(this.zaehler), Math.abs(this.nenner));
+	 * 
+	 * if(commonDivisor !=0){
+	 * this.zaehler /= commonDivisor;
+	 * this.nenner /= commonDivisor;
+	 * }
+	 * 
+	 * if(this.nenner < 0) {
+	 * this.zaehler *= -1;
+	 * this.nenner *= -1;
+	 * }
+	 * 
+	 * 
+	 * }
+	 */
 
 	// helper method Euklidian algorithm ,
 	// here are no mistakes
--- src/test/java/Homework6/exercise2/FractionTest.java
+++ src/test/java/Homework6/exercise2/FractionTest.java
@@ -12,6 +12,34 @@ public class FractionTest {
 
 		
 	}
+	@Test 
+	
+	void testKehrwert() {
+		Fraction f = new Fraction(2,3);
+		f.Kehrwert();
+		assertEquals(3, f.getZaehler(), "Kehrwert: Numerator should be 3 for original 2/3");
+		assertEquals(2, f.getNenner(),"Kehrwert: Denominator should be 2 for original 2/3" );
+	}
+	
+	@Test
+	void testMultiplikation() {
+		Fraction f1 = new Fraction(1,2);
+		Fraction f2 = new Fraction(2,3);
+		
+		f1.multiplikation(f2);
+		assertEquals(2, f1.getZaehler(), "Multiplikation: Numerator should be 1*2=2");
+		assertEquals(6, f1.getZaehler(), "Multiplikation: Denominator should be 2*3=6");
+
+	}
+	
+	@Test
+	void testKuerzen() {
+		Fraction f = new Fraction(6,9);
+		f.kuerzen();
+		assertEquals(2, f.getZaehler(), "Kuerzen: Numerator of 6/9 should become 2");
+		assertEquals(3, f.getNenner(), "Kuerzen: Numerator of 6/9 should become 3");
+		
+	}
 	
 }
 
